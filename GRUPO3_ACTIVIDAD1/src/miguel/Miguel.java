dbd31b9d7ce1b908a58583f6b44ab989dd714849
Miguel Garcia <ggmiguel1995@gmail.com>
2023-02-04 17:15:00 +0100

    Primer commit real

--- GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
+++ GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
@@ -5,6 +5,8 @@ public class Miguel {
 	public static void main(String[] args) {
 		System.out.println("Soy miguel y esta es la primera actividad de git");
 
+		// me adelante al primer punto y escribi mi nombre antes de tiempo
+		System.out.println("Hola, soy Miguel");
 	}
 
 }
