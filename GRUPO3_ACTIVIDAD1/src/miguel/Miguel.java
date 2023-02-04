832d3593c2713bd52cd592dabd2888c69d471ddb
Miguel Garcia <ggmiguel1995@gmail.com>
2023-02-04 17:45:39 +0100

    añadido nombre

--- GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
+++ GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
@@ -5,7 +5,7 @@ public class Miguel {
 	public static void main(String[] args) {
 		System.out.println("Soy miguel y esta es la primera actividad de git");
 
-		// me adelante al primer punto y escribi mi nombre antes de tiempo
+		// me adelanté al primer punto y escribí mi nombre antes de tiempo
 		System.out.println("Hola, soy Miguel");
 	}
 
