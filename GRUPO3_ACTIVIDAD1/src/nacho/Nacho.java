5310a484cdb1d96ab5d1656040fa913cac7539b6
Ignacio.MartinRoque <martinroque.ignacio>
2023-02-05 23:05:26 +0100

    cambio en las comidas

--- GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
+++ GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
@@ -5,6 +5,7 @@ public class Nacho {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Nacho este es mi primer commit en la rama develop y en git");
 		System.out.println("Mis aficiones son el hockey, harry potter y jugar al wow");
+		System.out.println("Mis comidas favoritas son; La pizza, la hamburguesa y la fabada");
 	}
 
 }
