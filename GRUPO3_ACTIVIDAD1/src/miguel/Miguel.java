8c05d3e6b1f357d712269a5e5d3384bf169760df
Miguel <123322511+MGarcia-95@users.noreply.github.com>
2023-02-05 10:50:04 +0100

    Update Miguel.java

--- GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
+++ GRUPO3_ACTIVIDAD1/src/miguel/Miguel.java
@@ -7,6 +7,13 @@ public class Miguel {
 
 		// me adelanté al primer punto y escribí mi nombre antes de tiempo
 		System.out.println("Hola, soy Miguel");
+		hobbies();
 	}
+	public static void hobbies() {
+		System.out.println("Mis tres hobbies favoritos son: ");
+		System.out.println("- Montar en bici");
+		System.out.println("- Jugar al counter");
+		System.out.println("- Subir montañas con piedras en la espalda");
+		}
 
 }
