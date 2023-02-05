6a00039a06961d941c26085cba571be8b89e107b
Ignacio.MartinRoque <martinroque.ignacio>
2023-02-05 21:11:15 +0100

    Segunda version de las tareas

--- GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
+++ GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
@@ -4,7 +4,7 @@ public class Nacho {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Nacho este es mi primer commit en la rama develop y en git");
-
+		System.out.println("Mis aficiones son el hockey, harry potter y jugar al wow");
 	}
 
 }
