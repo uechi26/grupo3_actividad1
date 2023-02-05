881eeb5af0e4c2759328d6ad36cd807794d2234d
uechi26 <82774438+uechi26@users.noreply.github.com>
2023-02-05 22:45:04 +0100

    cambios 3

--- GRUPO3_ACTIVIDAD1/src/Donato/Donato.java
+++ GRUPO3_ACTIVIDAD1/src/Donato/Donato.java
@@ -9,5 +9,6 @@ public class Donato {
 	public static void nombre() {
 	System.out.println("Hola soy Donato");
 	System.out.println("Mis hobbies son : Escuchar musica, ver peliculas, caminar con mi perro");
+	System.out.println("Mis 3 comidas favoritas son carnivoras, hamburguesas, entrecot y costillas");
 	}
 }
