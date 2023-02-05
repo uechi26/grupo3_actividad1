41ccfbabeae50255b2c4bd41e2a99d323a96eb6f
uechi26 <82774438+uechi26@users.noreply.github.com>
2023-02-05 23:07:48 +0100

    cambios 4

--- GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
+++ GRUPO3_ACTIVIDAD1/src/nacho/Nacho.java
@@ -5,6 +5,8 @@ public class Nacho {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Nacho este es mi primer commit en la rama develop y en git");
 		System.out.println("Mis aficiones son el hockey, harry potter y jugar al wow");
+		
+		System.out.println("A nacho le gusta la comida china, japonesa y mexicana");
 	}
 
 }
