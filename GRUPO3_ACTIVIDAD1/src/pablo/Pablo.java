e687251173985557417c25e932a965ee3787e74d
Pablo <pablootero.work@proton.me>
2023-02-05 12:15:53 +0100

    Añadidos los hobbies

--- GRUPO3_ACTIVIDAD1/src/pablo/Pablo.java
+++ GRUPO3_ACTIVIDAD1/src/pablo/Pablo.java
@@ -4,6 +4,7 @@ public class Pablo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Pablo y este es mi primer commit a la rama develop");
+		System.out.println("Hola de nuevo, soy Pablo y mis aficiones son leer, montar en bici y programar en eclipse");
 	}
 
 }
